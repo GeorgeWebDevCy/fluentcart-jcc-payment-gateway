d0142a14b1c05cd89f915a29b270cdc42bdbe2b9
George Nicolaou <orionas.elite@gmail.com>
2025-10-18 19:13:10 +0300

    updater added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '97d1559e06ec87531209e88586393995df37a1d2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '97d1559e06ec87531209e88586393995df37a1d2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yahnis-elsts/wp-update-server' => array(
+            'pretty_version' => 'v2.0.2',
+            'version' => '2.0.2.0',
+            'reference' => '043380dd45be4620b82d8906e40bceb0955359f5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yahnis-elsts/wp-update-server',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
